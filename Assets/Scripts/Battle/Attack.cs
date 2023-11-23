6164ba8412e4914b1e6fcb59eca6459d736e57c8
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-11-23 12:02:23 +0100

    Player Attack choice enable

--- Assets/Scripts/Battle/Attack.cs
+++ Assets/Scripts/Battle/Attack.cs
@@ -8,7 +8,12 @@ public class Attack : MonoBehaviour
 {
     public Button attackButton;
     public TextMeshProUGUI turnText;
+    [Header("Player Battle UI")]
+    [SerializeField] private GameObject _attackLevel1;
+    [SerializeField] private GameObject _attackLevel2, _attackLevel4, _attackLevel6, _defense;
 
+    [Header("Player")]
+    [SerializeField] private GameObject _player;
 
     [Header("UI elements attack order")]
     [SerializeField] private GameObject _whatEnemy;
@@ -25,4 +30,62 @@ public class Attack : MonoBehaviour
         yield return null;
     }
 
+
+    public void ChangeAttackListFromLevelUp()
+    {
+        //TODO: This function called when attack to check what attacks the player has unlocked
+        PlayerClass l_player = _player.GetComponent<PlayerClass>();
+        switch (l_player.playerLevel)
+        {
+            case 1:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(false);
+                _attackLevel4.SetActive(false);
+                _attackLevel6.SetActive(false);
+                _defense.SetActive(true);
+                break;
+            case 2:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(false);
+                _attackLevel6.SetActive(false);
+                _defense.SetActive(true);
+                break;
+            case 3:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(false);
+                _attackLevel6.SetActive(false);
+                _defense.SetActive(true);
+                break;
+            case 4:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(true);
+                _attackLevel6.SetActive(false);
+                _defense.SetActive(true);
+                break;
+            case 5:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(true);
+                _attackLevel6.SetActive(false);
+                _defense.SetActive(true);
+                break;
+            case 6:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(true);
+                _attackLevel6.SetActive(true);
+                _defense.SetActive(true);
+                break;
+            case >= 7:
+                _attackLevel1.SetActive(true);
+                _attackLevel2.SetActive(true);
+                _attackLevel4.SetActive(true);
+                _attackLevel6.SetActive(true);
+                _defense.SetActive(true);
+                break;
+        }
+    }
 }
