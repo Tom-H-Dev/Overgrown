08fcd48ab7082298d9d1fd6c94d7e80911e91c33
MaxWesterveld <210402@student.glu.nl>
2023-12-01 14:29:19 +0100

    Main + End Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public static MainMenu instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
+    public void ToScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
