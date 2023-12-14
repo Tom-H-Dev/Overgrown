744adecb68db7fb020c98eb0260eaa99339c57ae
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-12-14 12:13:01 +0100

    player movement fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private float _speed = 5;
-    public bool grounded = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,7 +16,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if (GameManager.instance.canMovePlayer)
         {
-            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed , 0 , Input.GetAxis("Vertical") * _speed);
+            rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed , rb.velocity.y , Input.GetAxis("Vertical") * _speed);
 
 
             if (Input.GetKeyDown(InputManager.instance.keyBinds.CheckKey("sprint")))
