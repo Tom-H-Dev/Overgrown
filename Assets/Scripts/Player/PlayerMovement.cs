bad0768566bba5bf2feb49f74d5c9d7a7b70b973
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-12-14 11:45:03 +0100

    player movement start fix

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody rb;
     private float _speed = 5;
+    bool grounded = false;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -17,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
         if (GameManager.instance.canMovePlayer)
         {
             rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed , 0 , Input.GetAxis("Vertical") * _speed);
+
+
             if (Input.GetKeyDown(InputManager.instance.keyBinds.CheckKey("sprint")))
             {
                 _speed = 7;
@@ -25,6 +28,25 @@ public class PlayerMovement : MonoBehaviour
             {
                 _speed = 5;
             }
+
+            if (!grounded)
+            {
+                rb.velocity = new Vector3(Input.GetAxis("Horizontal") * _speed, -2, Input.GetAxis("Vertical") * _speed);
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            grounded = false;
         }
     }
 }
