ad63b30b6ce18d5d45570ddc74115c3887762509
Tom-H-Dev <tom.holewijn@protonmail.com>
2023-12-15 23:11:43 +0100

    beta fix

--- Assets/Scripts/Battle/StartBattleManager.cs
+++ Assets/Scripts/Battle/StartBattleManager.cs
@@ -30,14 +30,14 @@ public class StartBattleManager : MonoBehaviour
         if (!hasEncountered)
         {
             GameManager.instance.canMovePlayer = false;
-            _turnManager.SetActive(true);
-            _attackScript.AttackButton();
-            _combatMenuStuff.SetActive(true);
 
             _enemy1.SetActive(true);
             _enemy2.SetActive(true);
             _attackStartButton.interactable = true;
 
+            _turnManager.SetActive(true);
+            _attackScript.AttackButton();
+            _combatMenuStuff.SetActive(true);
 
 
             for (int i = 0; i < TurnManager.instance.currentActiveEnemies.Count; i++)
